bb87b786a7815ffd9d2ab5a48ca06a7dc3745df9
Emma Wang <112578755+Emmawang00@users.noreply.github.com>
2023-04-26 00:47:11 +0000

    deleting files

--- examples/quickstart/query.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
-   -H "content-type: application/json" \
-   --data "[[5.9, 3, 5.1, 1.8]]" \
-   http://127.0.0.1:3000/classify
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-source /home/codespace/venv/bin/activate
-#append it to bash so every shell launches with it 
-echo 'source /home/codespace/venv/bin/activate' >> ~/.bashrc
